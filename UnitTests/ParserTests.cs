ba157b388ccce982be5e3fdfcfa809843056613a
vahidkianfar <vahidkianfar@gmail.com>
2022-07-14 22:15:52 +0100

    Passed the first Test, Reading CSV File.

--- UnitTests/ParserTests.cs
+++ UnitTests/ParserTests.cs
@@ -1,15 +1,37 @@
+using FluentAssertions;
 namespace UnitTests;
 
 public class Tests
 {
+    private static readonly DirectoryInfo? _datasetPath =
+        Directory.GetParent(Directory.GetParent(
+            Directory.GetParent(
+                Directory.GetParent(
+                    Directory.GetCurrentDirectory())?
+                    .ToString() ?? string.Empty)?.ToString()
+            ?? string.Empty)?.ToString() ?? string.Empty);
+    
+    //string text = File.ReadAllText(_datasetPath + "/CSV-Parser/Dataset/input.csv");
     [SetUp]
     public void Setup()
     {
+
+    
     }
 
     [Test]
-    public void Test1()
+    public void Read_Dataset_and_Store_It_Into_String()
     {
-        Assert.Pass();
+        var text = File.ReadAllText(_datasetPath + "/CSV-Parser/Dataset/input.csv");
+        text.Should().NotBeNull();
     }
+    
+    [Test]
+    public void Read_Dataset_and_Store_It_Into_String_And_Check_If_It_Is_Not_Empty()
+    {
+        var text = File.ReadAllText(_datasetPath + "/CSV-Parser/Dataset/input.csv");
+        text.Should().NotBeEmpty();
+    }
+    
+    
 }
