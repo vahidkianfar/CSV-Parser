5a979ca0f4dcfb1b9c062f59a2cfb0f199a0f8c1
vahidkianfar <vahidkianfar@gmail.com>
2022-07-15 17:51:22 +0100

    Fixed some Bugs in Search Menu.

--- CSV-Parser/UI/SearchMenu.cs
+++ CSV-Parser/UI/SearchMenu.cs
@@ -13,11 +13,12 @@ public static class SearchMenu
             Console.Clear();
             var selectInstructionOption =
                 ConsoleHelper.MultipleChoice(true, "1. Search by First Name and Last Name", "2. Search by County" , "3. Search by Company Name",
-                    "4. Search by House Number Digits" , "5. Search by the least URL Characters", "6. Search By the Postal Code Digits",
-                    "7. Return the Persons by bigger first phone number","8. Exit");
+                    "4. Retrieve Persons by House Number Digits (3 digits)" , "5. Search by the least URL Characters", "6. Retrieve Persons by Postal Code Digits (1 digits)",
+                    "7. Retrieve Persons by bigger first phone number","8. Exit");
             switch (selectInstructionOption)
             {
                 case 0:
+                    Console.Clear();
                     var firstName = AnsiConsole.Ask<string>("Enter First Name (e.g. [orange1]Karma[/]): ");
                     var lastName = AnsiConsole.Ask<string>("Enter Last Name (e.g. [orange1]Quarto[/]): ");
                     Console.Clear();
@@ -26,37 +27,37 @@ public static class SearchMenu
                     break;
                 
                 case 1:
-                    var county = AnsiConsole.Ask<string>("Enter the County (e.g. [orange1]Derbyshire[/]): ");
                     Console.Clear();
+                    var county = AnsiConsole.Ask<string>("Enter the County (e.g. [orange1]Derbyshire[/]): "); ;
                     await Person.RetrievePersonByCounty(persons, county);
                     PressAnyKeyToContinue();
                     break;
                 
                 case 2:
-                    var company = AnsiConsole.Ask<string>("Enter the Company Name \"Full or Partial\" (e.g. [orange1]Esq[/]): ");
                     Console.Clear();
+                    var company = AnsiConsole.Ask<string>("Enter the Company Name \"Full or Partial\" (e.g. [orange1]Esq[/]): ");
                     await Person.RetrievePersonByCompanyName(persons, company);
                     PressAnyKeyToContinue();
                     break;
                 
                 case 3:
-                    var houseNumberDigits = AnsiConsole.Ask<int>("Enter the House Number Digits (e.g. [orange1]3[/]): ");
+                    //var houseNumberDigits = AnsiConsole.Ask<int>("Enter the House Number Digits (e.g. [orange1]3[/]): ");
                     Console.Clear();
-                    await Person.RetrievePersonByHouseNumberDigits(persons, houseNumberDigits);
+                    await Person.RetrievePersonByHouseNumberDigits(persons, 3);
                     PressAnyKeyToContinue();
                     break;
                 
                 case 4:
-                    var urlCharacters = AnsiConsole.Ask<int>("Enter the least URL Characters (e.g. [orange1]35[/]): ");
                     Console.Clear();
+                    var urlCharacters = AnsiConsole.Ask<int>("Enter the least URL Characters (e.g. [orange1]35[/]): ");
                     await Person.RetrievePersonByURL(persons, urlCharacters);
                     PressAnyKeyToContinue();
                     break;
                 
                 case 5:
-                    var postalCodeDigits = AnsiConsole.Ask<int>("Enter the Postal Code Digits (e.g. [orange1]1[/]): ");
+                    //var postalCodeDigits = AnsiConsole.Ask<int>("Enter the Postal Code Digits (e.g. [orange1]1[/]): ");
                     Console.Clear();
-                    await Person.RetrievePersonByPostalCodeNumberOfDigits(persons, postalCodeDigits);
+                    await Person.RetrievePersonByPostalCodeNumberOfDigits(persons, 1);
                     
                     PressAnyKeyToContinue();
                     break;
@@ -80,5 +81,6 @@ public static class SearchMenu
         Console.WriteLine("Press any key to continue...");
         Console.ReadKey();
         Console.ResetColor();
+        Console.Clear();
     }
 }
